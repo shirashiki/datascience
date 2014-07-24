b2bc5684e110f3847cb5b39d26e834d61b728f68
shirashiki <silvio.hirashiki@gmail.com>
2014-07-24 11:11:52 -0400

    stable version

--- rproject/run_analysis.R
+++ rproject/run_analysis.R
@@ -1,6 +1,8 @@
 ## Getting and Cleaning Data July 2014
 #  Course Project
 
+## 2, 1, 3 
+
 ## Tasks
 # 1     Merges the training and the test sets to create one data set.
 # 2     Extracts only the measurements on the mean and standard deviation 
@@ -25,7 +27,6 @@ projectTasks <- function() {
                 File ./UCI HAR Dataset/activity_labels.txt not found.")
     }
     
-    
     if (!file.exists("./UCI HAR Dataset/test/X_test.txt")) {
         stop("Check if the data is in your working directory. 
                 File ./UCI HAR Dataset/test/X_test.txt not found.")
@@ -45,9 +46,7 @@ projectTasks <- function() {
         stop("Check if the data is in your working directory. 
                 File ./UCI HAR Dataset/train/y_train.txt not found.")
     }
-    
-    
-    
+
     if (!file.exists("./UCI HAR Dataset/test/subject_test.txt")) {
         stop("Check if the data is in your working directory. 
                 File ./UCI HAR Dataset/test/subject_test not found.")
@@ -58,21 +57,20 @@ projectTasks <- function() {
                 File ./UCI HAR Dataset/train/subject_train.txt not found.")
     }
     
+    
     ## ---------------------------------------------------------------
-    ## Loads training and test sets
-    ## Here I load and adjust Training and Test Data
+    ## Loads training and test sets 
     
     # Loads features
     features <- read.table(file="./UCI HAR Dataset/features.txt",
                          header=FALSE, sep="")
-    
-    # defines features to keep
-    keeps <- grep("*-mean|-std*", features[,2])
-    
+
     # puts meaningful column names
     colnames(features)[1] = "FeaturePosition"
     colnames(features)[2] = "FeatureName"
     
+    # defines features to keep
+    keeps <- grep("*-mean|-std*", features[,2])
     
     # Loads activity labels
     activityLabels <- read.table(file="./UCI HAR Dataset/activity_labels.txt",
@@ -82,7 +80,7 @@ projectTasks <- function() {
     colnames(activityLabels)[2] = "ActivityName"
     
     
-    ##-----  Prepares Training data
+    ##----- Prepares Training data
     # Loads Training labels
     trainingLabels <- read.table(file="./UCI HAR Dataset/train/y_train.txt",
                            header=FALSE, sep="")
@@ -96,27 +94,23 @@ projectTasks <- function() {
     # in the feature.txt file
     colnames(trainingSet) <- features$FeatureName
     
-    # Keeps only the columns related to mean and standard deviation
+    # Project Tasks:
+    # 2 - Extracts only the measurements on the mean and standard deviation 
+    #     for each measurement.
     trainingSet <- trainingSet[,keeps]
-    
-    # Loads subjects
-    trainingSubjects <- read.table(file="./UCI HAR Dataset/train/subject_train.txt",
-                                 header=FALSE, sep="")
-    colnames(trainingSubjects)[1] <- "Subject"
-    
-    # Merges to add subject information
-    trainingSet <- cbind(trainingSubjects, trainingSet)
     
     # Merges to name each activity in the data set
     trainingSet <- cbind(trainingLabels, trainingSet)
     
-    # Adds the name of the set, so we can merge Training and Test later
-    SetName = rep("Training Set", nrow(trainingSet))
-    trainingSet <- cbind(SetName, trainingSet)
+    # Loads subjects
+    trainingSubjects <- read.table(file="./UCI HAR Dataset/train/subject_train.txt",
+                                   header=FALSE, sep="")
+    colnames(trainingSubjects)[1] <- "Subject"
+    # Merges to add subject information
+    trainingSet <- cbind(trainingSubjects, trainingSet)
     
     
-    
-    ##-----  Prepares Test data
+    ##----- Prepares Test data
     # Loads Test labels
     testLabels <- read.table(file="./UCI HAR Dataset/test/y_test.txt",
                                  header=FALSE, sep="")
@@ -130,47 +124,61 @@ projectTasks <- function() {
     # in the feature.txt file
     colnames(testSet) <- features$FeatureName
     
-    # Keeps only the columns related to mean and standard deviation
+    # Project Tasks:
+    # 2 - Extracts only the measurements on the mean and standard deviation 
+    #     for each measurement.
     testSet <- testSet[,keeps]
+    
+    # Merges to name each activity in the data set
+    testSet <- cbind(testLabels, testSet)
     
     # Loads subjects
     testSubjects <- read.table(file="./UCI HAR Dataset/test/subject_test.txt",
-                                   header=FALSE, sep="")
+                               header=FALSE, sep="")
     colnames(testSubjects)[1] <- "Subject"
     
     # Merges to add subject information
     testSet <- cbind(testSubjects, testSet)
     
-    # Merges to name each activity in the data set
-    testSet <- cbind(testLabels, testSet)
+    ## ---------------------------------------------------------------
+    ## Bind Training and Test Sets, generate files
+    ## Also here we put meaningful column names
     
-    # Adds the name of the set, so we can merge Training and Test later
-    SetName = rep("Test Set", nrow(testSet))
-    testSet <- cbind(SetName, testSet)
-    
-    
-    ## --- Merge to generate final data
-    ## At this point we have Training and Test data frames prepared
-    
+    # Project Tasks:
+    # 1 - Merges the training and the test sets to create one data set.
     allData <- rbind(trainingSet, testSet)
+    
+    # Project Tasks:
+    # 3 - Uses descriptive activity names to name the activities in the data set
+    # Merge to add Activity Labels
     allData <- merge(activityLabels, allData, 
                                 by.x="ActivityID",
                                 by.y="ActivityID",
                                 sort=FALSE,
                                 all=TRUE)
     
+    # Project Tasks:
+    # 4 - Appropriately labels the data set with descriptive variable names. 
     
-    ## need to tidy column names before generating final file
+    # Drops remaining unwanted columns
+    drops <- c("ActivityID")
+    allData <- allData[,!(names(allData) %in% drops)]
     
-    # ZZZZ CHECK THIS summary data frame
-    sum_DF <- aggregate(allData, 
-                        by=list(allData$Subject, allData$ActivityName),
+    # Renames columns setting descriptive names
+    
+    
+    
+
+    summaryData <- aggregate(allData, 
+                        by=list(allData$ActivityName, allData$Subject),
                         FUN=mean, 
                         na.rm=TRUE)
     
-    write.table(sum_DF, file="./cleanData.txt",
-                append = FALSE,
-                sep="\t", col.names = TRUE)
+    write.table(summaryData, file="./cleanData.txt",
+                append=FALSE,
+                sep="\t",
+                row.names=FALSE, 
+                col.names=TRUE )
     
     #PENDING:
     # We have 30 subjects in total, so no need to use SetName. Use only
